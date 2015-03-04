70ba6f683c844ded4a9f5dd7cb87fbb37bf05ccd
Robert Xiu <xiurobert@gmail.com>
2015-03-04 14:24:32 +0800

    ##MAJORCHANGE##

---
+++ src/main/java/net/towerwarz/towers/GatlingGun/GatlingGunCoreData/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Created by xiurobert on 3/3/2015.
+ */
+
+/**
+ * Gatling Gun Core Data
+ * Used to render the Gatling Gun and its resources
+ * (src/main/resources)
+ */
+package net.towerwarz.towers.GatlingGun.GatlingGunCoreData;
---
+++ src/main/java/net/towerwarz/towers/GatlingGun/GatlingGunLv3.java
@@ -0,0 +1,9 @@
+package net.towerwarz.towers.GatlingGun;
+
+/**
+ * Created by xiurobert on 2/27/2015.
+ */
+
+public class GatlingGunLv3 {
+    public String def = "RefinedBarrelAndFiringMechanism";
+}
