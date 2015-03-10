d407cab3b96d3fdb983ff43a102b6efc0179b00c
BehChuenYang <depictiger@gmail.com>
2015-03-10 22:28:51 +0800

    updated file(pls resolve the import :P)

--- src/main/java/net/towerwarz/Mobs/ModernPack/Soldier.java
+++ src/main/java/net/towerwarz/Mobs/ModernPack/Soldier.java
@@ -1,4 +1,4 @@
-package 
+package net.towerwarz.towers.GatlingGun;
 import 
 /* sorry for Travis build errors, i dont know the get attack wave property :P
 */
@@ -7,6 +7,6 @@ public String def="'Your Average Joe' combatant";
 public int MobDataId=1
 public int MobHp=(mathPow(waveNumber,2)*100)/(mathPow(waveNumber,1.5))
 public int MoveSpeed=16 
-/* 16 is default move speed in roblox.(16 studs/sec, or 80cm/sec)(binary)
+/* 16 is default move speed in roblox.(16 studs/sec, or 5m/sec)(binary)
 */
-public int causeHarmToBase=5
+public int causeHarmToBase=1
