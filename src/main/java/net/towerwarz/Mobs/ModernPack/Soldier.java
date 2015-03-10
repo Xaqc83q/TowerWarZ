096af1ff1e05279d416f5d0adf144b8a4b3b366e
BehChuenYang <depictiger@gmail.com>
2015-03-10 22:24:43 +0800

    updated file(pls resolve the import :P)

---
+++ src/main/java/net/towerwarz/Mobs/ModernPack/Soldier.java
@@ -0,0 +1,12 @@
+package 
+import 
+/* sorry for Travis build errors, i dont know the get attack wave property :P
+*/
+public class Soldier{
+public String def="'Your Average Joe' combatant";
+public int MobDataId=1
+public int MobHp=(mathPow(waveNumber,2)*100)/(mathPow(waveNumber,1.5))
+public int MoveSpeed=16 
+/* 16 is default move speed in roblox.(16 studs/sec, or 80cm/sec)(binary)
+*/
+public int causeHarmToBase=5
