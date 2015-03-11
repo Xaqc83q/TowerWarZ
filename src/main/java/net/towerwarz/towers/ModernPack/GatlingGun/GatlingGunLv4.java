b047a3435bfdec2f93ed805aa0a683af9e293cd0
Robert Xiu <xiurobert@gmail.com>
2015-03-11 13:27:00 +0800

    Fix TRAVIS BUILDS BCY!!!

--- src/main/java/net/towerwarz/towers/ModernPack/GatlingGun/GatlingGunLv4.java
+++ src/main/java/net/towerwarz/towers/ModernPack/GatlingGun/GatlingGunLv4.java
@@ -1,5 +1,8 @@
-package net.towerwarz.towers.GatlingGun;
-import java.Util.random
-/*lol idk if its caps or not
-*/
+package net.towerwarz.towers.ModernPack.GatlingGun;
 
+import java.util.random; //Missing semicolon causing failed build here, BCY TAKE NOTE!
+
+
+public class GatlingGunLv4 {
+    
+}
