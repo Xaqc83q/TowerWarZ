cb0c192c041d9d952f7ab2a89968f6662289d1d6
BehChuenYang <depictiger@gmail.com>
2015-03-11 17:01:47 +0800

    this'll do for now

--- src/main/java/net/towerwarz/towers/AncientPack/SwordsmanLv1.java
+++ src/main/java/net/towerwarz/towers/AncientPack/SwordsmanLv1.java
@@ -1,10 +1,11 @@
 package net.towerwarz.towers.AncientPack;
-
 /**
  * Created by xiurobert on 3/5/2015
  * Made with IntelliJ IDEA
  */
  
 public class Swordsman {
+public String def = "Swordsman"
+ public int SwordsmanLv1TotalCost = 100
 }
   
