e66be70d1641b7ba1a216437317568d863f71d85
BehChuenYang <depictiger@gmail.com>
2015-03-11 16:59:38 +0800

    HOPEFULLY stopping Travis CI error builds in the mobs section

--- src/main/java/net/towerwarz/Mobs/ModernPack/Soldier.java
+++ src/main/java/net/towerwarz/Mobs/ModernPack/Soldier.java
@@ -1,7 +1,6 @@
 package net.towerwarz.towers.GatlingGun;
-import 
-/* sorry for Travis build errors, i dont know the get attack wave property :P
-*/
+//import
+//
 public class Soldier{
 public String def="'Your Average Joe' combatant";
 public int MobDataId=1
@@ -10,3 +9,4 @@ public int MoveSpeed=16
 /* 16 is default move speed in roblox.(16 studs/sec, or 5m/sec)(binary)
 */
 public int causeHarmToBase=1
+}
