1005d4b3ed308722c3a29e4493dceb257a239054
BehChuenYang <depictiger@gmail.com>
2015-03-24 19:07:54 +0800

    Create EnemyProperty.java

---
+++ src/main/java/net/towerwarz/Data/EnemyProperty.java
@@ -0,0 +1,13 @@
+package net.towerwarz.Data;
+//like always, i do not know the import.
+//
+public class EnemyBehavior{
+//well the name says it all
+//
+private EnemyWalkDirection(){
+public str targetDirection="North"
+public str
+//sorry idk how to plugin the directions if it is wrong dont blame me me new
+//
+}
+}
