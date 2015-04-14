11abd9fcc25c71e7fbde66eb1ae59fba03c7538d
BehChuenYang <depictiger@gmail.com>
2015-04-14 20:45:05 +0800

    Update Soldier.java

--- src/main/java/net/towerwarz/Mobs/ModernPack/Soldier.java
+++ src/main/java/net/towerwarz/Mobs/ModernPack/Soldier.java
@@ -7,6 +7,6 @@ public class Soldier{
     //public int MobHp=(mathPow(waveNumber,2)*100)/(mathPow(waveNumber,1.5)) //Have you realised this is BAD JAVA??? USE AN IDE!
     public int MoveSpeed = 16 
 // Putting movespeed here -> Useless
-// Unless LWJGL does something
+// Unless LWJGL does something( idk LWJGL is for game man. You know it has movespeed by playing MC)
     public int causeHarmToBase=1
 }
