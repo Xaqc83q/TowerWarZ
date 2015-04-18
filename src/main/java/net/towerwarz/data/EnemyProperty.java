5a1d3a92819d9a0ae33912c53927d19bf5b6c613
Robert Xiu <xiurobert@gmail.com>
2015-04-18 03:16:22 +0000

    Update #1

---
+++ src/main/java/net/towerwarz/data/EnemyProperty.java
@@ -0,0 +1,15 @@
+package net.towerwarz.Data;
+
+public class EnemyBehavior{
+
+    public EnemyWalkDirection(){
+        String targetDirection="North";
+        /*
+         * Rules Of Java
+         * 1. ALWAYS PUT A SEMICOLON TO TERMINATE A LINE!!! THIS IS TIME NUMBER #30000
+         * 2. ALWAYS SPELL FULLY! E.g. String!!!
+         * 3. Capitalization is IMPORTANT!! String and string are 2 DIFFERENT THINGS!!!
+         * 4. USLESS COMMENTS -> REMOVE
+         */
+    }
+}
