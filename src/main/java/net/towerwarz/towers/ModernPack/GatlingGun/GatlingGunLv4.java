4557a7c32170efda93e53a0cf357024e7fcb5ba5
BehChuenYang <depictiger@gmail.com>
2015-05-12 17:45:39 +0800

    +Feature

--- src/main/java/net/towerwarz/towers/ModernPack/GatlingGun/GatlingGunLv4.java
+++ src/main/java/net/towerwarz/towers/ModernPack/GatlingGun/GatlingGunLv4.java
@@ -4,5 +4,15 @@ import java.util.Random; //Fixed
 
 
 public class GatlingGunLv4 {
+    public String def = "InfraRed";
+    int maxDmg = 8;
+    int minDmg = 4;
+    Random rand = new Random();
+    public int GatlingGunLv4Dmg = rand.nextInt((maxDmg - minDmg) + 1) + minDmg;
+    public double GatlingGunLv4SplashRadius = 0.01;
+    public double GatlingGunLv4ReloadRate = 0.01;
+    boolean canDetectCamo = true
+
+    int spreadRadius=18;
     
 }
