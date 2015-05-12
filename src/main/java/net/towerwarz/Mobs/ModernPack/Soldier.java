bb06eecd2a3a51e4e236c73ccf92d7245042842c
BehChuenYang <depictiger@gmail.com>
2015-05-12 17:43:03 +0800

    fine i will get an IDE
    do not complain cos i will get eclipse

--- src/main/java/net/towerwarz/Mobs/ModernPack/Soldier.java
+++ src/main/java/net/towerwarz/Mobs/ModernPack/Soldier.java
@@ -3,7 +3,9 @@ package net.towerwarz.Mobs.ModernPack;
 public class Soldier{
     public String def="'Your Average Joe' combatant";
     public int MobDataId = 1;
+    public int MobHp=(mathPow(waveNumber*100,2.5))/(mathPow(waveNumber,1.25))
     //public int MobHp=(mathPow(waveNumber,2)*100)/(mathPow(waveNumber,1.5)) //Have you realised this is BAD JAVA??? USE AN IDE!
+    // Hey this would be great//
     public int MoveSpeed = 16 ;
 // Putting movespeed here -> Useless
 // Unless LWJGL does something( idk LWJGL is for game man. You know it has movespeed by playing MC)
