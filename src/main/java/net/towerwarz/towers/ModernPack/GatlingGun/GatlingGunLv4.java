39f76a1d8dd714184b73a59546f84787496cf296
BehChuenYang <depictiger@gmail.com>
2015-06-03 21:05:34 +0800

    Update GatlingGunLv4.java

--- src/main/java/net/towerwarz/towers/ModernPack/GatlingGun/GatlingGunLv4.java
+++ src/main/java/net/towerwarz/towers/ModernPack/GatlingGun/GatlingGunLv4.java
@@ -4,14 +4,14 @@ import java.util.Random; //Fixed
 
 
 public class GatlingGunLv4 {
-    public String def = "InfraRed";
+    public String def = "InfraRed Technology";
     int maxDmg = 8;
     int minDmg = 4;
     Random rand = new Random();
     public int GatlingGunLv4Dmg = rand.nextInt((maxDmg - minDmg) + 1) + minDmg;
     public double GatlingGunLv4SplashRadius = 0.01;
     public double GatlingGunLv4ReloadRate = 0.01;
-    public boolean canDetectCamo = true
+    public boolean canDetectCamo = true;
 
     int spreadRadius=18;
     
