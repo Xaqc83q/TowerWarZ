2f2d9a89882bdc7906c562bf35a8759b6521bdb6
Robert Xiu <xiurobert@gmail.com>
2015-06-06 13:31:43 +0800

    Fix Travis Errors

--- src/main/java/net/towerwarz/Mobs/ModernPack/Soldier.java
+++ src/main/java/net/towerwarz/Mobs/ModernPack/Soldier.java
@@ -3,7 +3,7 @@ package net.towerwarz.Mobs.ModernPack;
 public class Soldier{
     public String def="'Your Average Joe' combatant";
     public int MobDataId = 1;
-    public int MobHp=(mathPow(waveNumber*100,2.5))/(mathPow(waveNumber,1.25));
+    //public int MobHp=(mathPow(waveNumber*100,2.5))/(mathPow(waveNumber,1.25)); (very broekn)
     //public int MobHp=(mathPow(waveNumber,2)*100)/(mathPow(waveNumber,1.5)) //Have you realised this is BAD JAVA??? USE AN IDE!
     // Hey this would be great (I know, if done better -.-)
     public int MoveSpeed = 16 ; // Movespeed class
