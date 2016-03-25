cfd0c0f4b44d9f063b8da07149b040a1135b9c5a
Robert Xiu <xiurobert@gmail.com>
2016-03-25 19:26:58 +0800

    Work to add JDS

--- src/main/java/net/towerwarz/Weapon.java
+++ src/main/java/net/towerwarz/Weapon.java
@@ -11,7 +11,7 @@ public abstract class Weapon {
     private int damage;
 
     private double splashRadius;
-    private int spreadRadius;
+    private double spreadRadius;
     private double reloadRate;
     private boolean canDetectCamo;
 
@@ -31,6 +31,12 @@ public abstract class Weapon {
         return type;
     }
 
+    /**
+     * Sets the weapon type
+     *
+     * @param type A type of Weapon defined in WeaponType(Enum)
+     */
+
     public void setType(WeaponType type) {
         this.type = type;
     }
@@ -38,6 +44,12 @@ public abstract class Weapon {
     public String getDef() {
         return def;
     }
+
+    /**
+     * Sets the description of a weapon/mob
+     *
+     * @param def String of what to make the definition
+     */
 
     public void setDef(String def) {
         this.def = def;
@@ -55,24 +67,45 @@ public abstract class Weapon {
         return damage;
     }
 
+    /**
+     * Sets the damage value of a weapon in Damage per second
+     *
+     * @param damage Integer, Damage per second
+     */
     public void setDamage(int damage) {
         this.damage = damage;
     }
+
 
     public double getSplashRadius() {
         return splashRadius;
     }
 
+    /**
+     * Set a radius for splash damage to occur
+     *
+     *
+     * @param splashRadius How much splash damage will be dealt (factorised)
+     */
+
     public void setSplashRadius(double splashRadius) {
         this.splashRadius = splashRadius;
     }
 
-    public int getSpreadRadius() {
+    public double getSpreadRadius() {
         return spreadRadius;
     }
 
-    public void setSpreadRadius(int spreadRadius) {
-        this.spreadRadius = spreadRadius;
+    /**
+     * Sets a radius for spreading of projectiles/bullets
+     *
+     * @param spreadRadius How much spread you want (from 0.00 to 1.00)
+     */
+
+    public void setSpreadRadius(double spreadRadius) {
+        if (spreadRadius < 1.00 && spreadRadius > 0.00) {
+            this.spreadRadius = spreadRadius;
+        }
     }
 
     public double getReloadRate() {
