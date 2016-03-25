32630ef30d0a169841fcf27a299e1e104192ccea
Way Yan <wayyan.win@gmail.com>
2016-03-25 16:57:14 +0800

    Modified Soldier class

--- src/main/java/net/towerwarz/mobs/Soldier.java
+++ src/main/java/net/towerwarz/mobs/Soldier.java
@@ -7,12 +7,11 @@ package net.towerwarz.mobs;
 public class Soldier {
     public String def = "'Your Average Joe' combatant";
     public int MobDataId = 1;
-    //public int MobHp=(mathPow(waveNumber*100,2.5))/(mathPow(waveNumber,1.25)); (very broekn)
-    //public int MobHp=(mathPow(waveNumber,2)*100)/(mathPow(waveNumber,1.5)) //Have you realised this is BAD JAVA??? USE AN IDE!
-    // Hey this would be great (I know, if done better -.-)
-
-    // Dude please put the wave number stuffz.... I Really wanna implement MObHp now we have an RandInt class
-
+    public int MobHp = 10;
     public int MoveSpeed = 16 ; // Movespeed class
-    public int causeHarmToBase = 1; // Use spaces pls
+    public int damage = 1;
+
+    public Soldier() {
+
+    }
 }
